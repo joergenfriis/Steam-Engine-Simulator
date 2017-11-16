8cbfa39b3945be2cafbea89e8c44f4e165b08e7a
Joergen Friis <joergenfriis@msn.com>
2017-11-16 14:27:06 +0100

    Tilfoejet exception handling

--- model/model.py
+++ model/model.py
@@ -10,7 +10,7 @@
 # 127 er neutral stilling.
 # Tredje byte er 0 eller 1. 1 angiver at modellen skal resettes.
 #
-# Testet 27.06.2017 Joergen Friis
+# 16.11.2017 Joergen Friis
 #******************************************************************
 
 import smbus
@@ -18,17 +18,55 @@ import smbus
 bus = smbus.SMBus(1)
 
 def ModelStop():
-    print('model stop')
-    bus.write_i2c_block_data(0x52, 0, [0,127,0])
+    # Try up to 3 times on a faliure
+    Success = False
+    caught_exception = None
+    for _ in range (3):
+        try:
+            bus.write_i2c_block_data(0x52, 0, [0,127,0])
+            Success = True
+            break
+        except:
+            print("Unexpected error:", sys.exc_info() [0])
+            time.sleep(1)
+    if not Success:
+        Print("Failed after 3 retries")
+    if Success:
+        print('model stop')
     return -1
 
 def ModelRun(energi,gear):
-    print('model run, energi= {} og gear = {}'.format(energi,gear))
-    bus.write_i2c_block_data(0x52, 0, [energi,gear,0])
+    # Try up to 3 times on a faliure
+    Success = False
+    caught_exception = None
+    for _ in range (3):
+        try:
+            bus.write_i2c_block_data(0x52, 0, [energi,gear,0])
+            Success = True
+            break
+        except:
+            print("Unexpected error:", sys.exc_info() [0])
+            time.sleep(1)
+    if not Success:
+        Print("Failed after 3 retries")
+    if Success:
+        print('model run, energi= {} og gear = {}'.format(energi,gear))
     return -1
 
 def ModelReset():
-    print('model reset')
-    bus.write_i2c_block_data(0x52, 0, [0,127,1])
+    # Try up to 3 times on a faliure
+    Success = False
+    caught_exception = None
+    for _ in range (3):
+        try:
+            bus.write_i2c_block_data(0x52, 0, [0,127,1])
+            Success = True
+            break
+        except:
+            print("Unexpected error:", sys.exc_info() [0])
+            time.sleep(1)
+    if not Success:
+        Print("Failed after 3 retries")
+    if Success:
+        print('model reset')
     return -1
-
