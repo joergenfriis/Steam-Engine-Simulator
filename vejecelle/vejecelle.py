5bdfe2e340f4d00f3b5523807f50f709d07b4fb7
Joergen Friis <joergenfriis@msn.com>
2017-11-16 21:01:24 +0100

    Exception handling implementet

--- vejecelle/vejecelle.py
+++ vejecelle/vejecelle.py
@@ -8,20 +8,51 @@
 # Programmet sender et byte til Arduinoen, der styrer reset, og det modtager
 # et int fra arduinoen med den vejede masse i gram.
 #
+# Joergen Friis 16.11.17
 #****************************************************************************
 import smbus
 import time
+import sys
 
 bus = smbus.SMBus(1)
 
 def Reset_vejecelle():
-    bus.write_i2c_block_data(0x31,0,[1, 0, 0])
+    # Try up to 30 times on a faliure
+    Success = False
+    caught_exception = None
+    for _ in range (30):
+        try:
+            bus.write_i2c_block_data(0x31,0,[1, 0, 0])
+            Success = True
+            break
+        except:
+            print("Unexpected error:", sys.exc_info() [0])
+            time.sleep(1)
+    if not Success:
+        print("Failed after 30 retries")
+    if Success:
+        print("Vejecelle resat")
     return -1
 
 def Read_vejecelle():
-    data = bus.read_i2c_block_data(0x31,0)
-    reset = data[0]
-    msb = data[1]
-    lsb = data[2]
-    masse = msb * 256 + lsb
+    # Try up to 30 times on a faliure
+    Success = False
+    caught_exception = None
+    for _ in range (30):
+        try:
+            data = bus.read_i2c_block_data(0x31,0)
+            Success = True
+            break
+        except:
+            print("Unexpected error:", sys.exc_info() [0])
+            time.sleep(1)
+    if not Success:
+        print("Failed after 30 retries")
+    if Success:
+        reset = data[0]
+        msb = data[1]
+        lsb = data[2]
+        masse = msb * 256 + lsb
+        print("Masse paa vejebaandet aflaest")
     return masse
+
