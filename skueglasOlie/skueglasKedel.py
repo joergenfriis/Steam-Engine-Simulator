03766b71b1269c14614968b9a1dcdc93d57aa2ab
Joergen Friis <joergenfriis@msn.com>
2017-11-29 16:06:21 +0100

    python moduler tilfoejet

---
+++ skueglasOlie/skueglasKedel.py
@@ -0,0 +1,101 @@
+# skueglasKedel.py
+#
+# Modul, der styrer den Arduino, der regulerer vaeskestanden i skueglasset med kedelvand.
+#
+# Slave adresse 0x37
+#
+# Programmet sender et heltal til Arduinoen:
+# 0-100 angiver hvor mange % glasset skal vaere fyldt.
+# 110 angiver at glasset skal fyldes yderligere 1 %
+# 111 angiver at glasset skal draenes for 1 %
+# 112 angiver at skueglasset skal resettes til 0 %
+#
+# Joegen Friis 29.11.2017
+#****************************************************************************
+
+import smbus
+import time
+import sys
+
+bus = smbus.SMBus(1)
+
+def set(niveau):
+    # Try up to 30 times on a failure
+    Success = False
+    caught_exception = None
+    for _ in range (30):
+        try:
+            bus.write_i2c_block_data(0x37,0,[niveau]) # send niveau til arduinoen
+            # if we get here, we succeeded, so break out of the loop
+            Success = True
+            break
+        except:
+            print("Unexpected error:", sys.exc_info() [0])
+            # wait a second for the retry
+            time.sleep(1)
+    if not Success:
+        print("Failed after 30 retries")
+    if Success:
+        print("Skueglas kedel ",niveau," %")
+    return -1
+
+
+def plus1():
+    # Try up to 30 times on a failure
+    Success = False
+    caught_exception = None
+    for _ in range (30):
+        try:
+            bus.write_i2c_block_data(0x37,0,[110]) # send 110 til arduinoen
+            # if we get here, we succeeded, so break out of the loop
+            Success = True
+            break
+        except:
+            print("Unexpected error:", sys.exc_info() [0])
+            # wait a second for the retry
+            time.sleep(1)
+    if not Success:
+        print("Failed after 30 retries")
+    if Success:
+        print("Skueglas kedel plus 1 %")
+    return -1
+
+def minus1():
+    # Try up to 30 times on a failure
+    Success = False
+    caught_exception = None
+    for _ in range (30):
+        try:
+            bus.write_i2c_block_data(0x37,0,[111]) # send 111 til arduinoen
+            # if we get here, we succeeded, so break out of the loop
+            Success = True
+            break
+        except:
+            print("Unexpected error:", sys.exc_info() [0])
+            # wait a second for the retry
+            time.sleep(1)
+    if not Success:
+        print("Failed after 30 retries")
+    if Success:
+        print("Skueglas kedel minus 1 %")
+    return -1
+
+def reset():
+    # Try up to 30 times on a failure
+    Success = False
+    caught_exception = None
+    for _ in range (30):
+        try:
+            bus.write_i2c_block_data(0x37,0,[112]) # send 112 til arduinoen
+            # if we get here, we succeeded, so break out of the loop
+            Success = True
+            break
+        except:
+            print("Unexpected error:", sys.exc_info() [0])
+            # wait a second for the retry
+            time.sleep(1)
+    if not Success:
+        print("Failed after 30 retries")
+    if Success:
+        print("Skueglas kedel resat")
+    return -1
