3cf0a0a3207cd9e4ca3a2d296d1f1931dc22137b
Joergen Friis <joergenfriis@msn.com>
2017-12-12 12:41:46 +0100

    Add files via upload

---
+++ sekundaerLuft/sekundaerLuft.py
@@ -0,0 +1,42 @@
+# sekundaerLuft.py
+#
+# modul, der aflaeser det sekundaere luftspjaelds stilling.
+#
+# Slaveadresse er 0x33
+#
+# Programmet laeser en streng med 1 byte fra Arduinoen.
+# Vaerdien er mellem 0 og 100, og angiver hvor mange procent
+# primaerluftspjaeldet er aabent.
+#
+# Joergen Friis 12.12.2017
+#
+################################################################################
+
+import smbus
+import time
+import sys
+
+
+bus = smbus.SMBus(1)
+
+def Read_sekundaerluft():
+    # Try up to 30 times on a failure
+    Success = False
+    caught_exception = None
+    for _ in range (30):
+        try:
+            data = bus.read_i2c_block_data(0x33, 0)
+            Success = True
+            break
+        except:
+            print("Unexpected error: ",sys.exc_info() [0])
+            time.sleep(1)
+    if not Success:
+        print("Failed after 30 retries")
+    if Success:
+        sekundaerluft = data[0]
+        print("Sekundaer luft = ",sekundaerluft," %")
+    return sekundaerluft
+
+            
+    
