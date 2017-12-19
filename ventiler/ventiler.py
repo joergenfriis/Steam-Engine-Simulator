08e7eee3c4002cd649197291f5dfbbabbbddfd94
Joergen Friis <joergenfriis@msn.com>
2017-12-19 14:25:03 +0100

    Add files via upload

---
+++ ventiler/ventiler.py
@@ -0,0 +1,15 @@
+# ventiler.py
+#
+# modul, der henter stillingen af de 6 manoevreventiler i % aaben:
+# damp ind
+# damp ud
+# kondensator ind
+# kondensator ud
+# kedelvand ind
+# kedelvand ud
+#
+# Slave nr 0x36
+#
+# Joergen Friis 19.12.2017
+#
+################################################################################
