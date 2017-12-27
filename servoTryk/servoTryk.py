ffbb44fbc4d1b3f8153247f3fa675efbf299b82f
Joergen Friis <joergenfriis@msn.com>
2017-12-27 16:30:18 +0100

    Add files via upload

---
+++ servoTryk/servoTryk.py
@@ -0,0 +1,41 @@
+# servoTryk.py
+#
+# modul, der styrer manometre for damptryk og kondensatorvacuum.
+#
+#Slaveadressen er 0x43
+#
+# Programmet sendet to heltal til Arduinoen:
+# Foerste tal angiver hvor mange grader motoren i vacuummeteret skal dreje.
+# Andet tal angiver hvor mange grader motoren i kedelomanometeret skal dreje.
+#
+# Naar manometrene er kalibreret aendres programmerne, saadan at de variable
+# angiver trykket.
+#
+#***************************************************************************
+# Joergen Friis 27.12.2017
+#***************************************************************************
+
+import smbus
+import time
+import sys
+
+bus = smbus.SMBus(1)
+
+def kondensator(vacuum,tryk):
+    Success = False
+    caught_exception = None
+    for _ in range (30):
+        try:
+            bus.write_i2c_block_data(0x43,0,[vacuum,tryk])
+            Success = True
+            break
+        except:
+            print("Unexpected error:", sys.exc_info() [0])
+            time.sleep(1)
+    if not Success:
+        print("Failed after 30 retries")
+    if Success:
+        print("Vacuum = ",vacuum,", tryk = ",tryk)
+    return -1
+
+
