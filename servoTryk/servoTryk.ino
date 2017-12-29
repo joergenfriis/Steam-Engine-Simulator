e603ff89fdbd9534accebf7c118b63b0b21ecb1e
Joergen Friis <joergenfriis@msn.com>
2017-12-29 10:26:38 +0100

    Add files via upload

--- servoTryk/servoTryk.ino
+++ servoTryk/servoTryk.ino
@@ -14,20 +14,15 @@ Joergen Friis 27.12.2017
 #include <Servo.h>
 #include <Wire.h>
 #define SLAVE_ADDRESS 0x43
-#define REG_MAP_SIZE 2
-#define MAX_SEND_BYTES 2
+#define REG_MAP_SIZE 3
+#define MAX_SEND_BYTES 3
 
 byte registerMap[REG_MAP_SIZE];
-
-
-
-
-
 
 Servo dampServo;
 Servo kondensServo;
 
-int tryk = 0;
+int tryk = 50;
 int vacuum = 0;
 
 
@@ -36,22 +31,30 @@ void setup()
   Wire.begin(SLAVE_ADDRESS);
   Wire.onReceive(receiveEvent);
   
-  dampServo.attach(9);
-  kondensServo.attach(10);
+  Serial.begin(9600);
+  
+  dampServo.attach(10);
+  kondensServo.attach(9);
   
   registerMap[0] = 0;
   registerMap[1] = 0;
+  registerMap[2] = 50;
 }
 
 void loop()
 {
-  tryk = registerMap[0];
-  tryk = map(tryk, 0, 100, 0, 180);
+  delay(3000);
+  tryk = registerMap[2];
+  Serial.print("Tryk = ");
+  Serial.println(tryk);
   dampServo.write(tryk);
   delay(15);
   
   vacuum = registerMap[1];
-  vacuum = map(vacuum, 0, 100, 0, 180);
+  //vacuum = map(vacuum, 0, 100, 0, 180);
+  Serial.print("Vacuum = ");
+  Serial.println(vacuum);
+  Serial.println();
   kondensServo.write(vacuum);
   delay(15);
 }
@@ -64,3 +67,4 @@ void receiveEvent(int byteCount)
     registerMap[i] = c;
   }
 }
+
