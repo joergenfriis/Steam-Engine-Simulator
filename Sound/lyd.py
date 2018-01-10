98c7c4675167b589c250f27a6bcdd7d7677ca816
Joergen Friis <joergenfriis@msn.com>
2018-01-10 11:47:22 +0100

    Add files via upload

---
+++ Sound/lyd.py
@@ -0,0 +1,15 @@
+# lyd.py
+#
+# modul, der indeholder referencer til alle lydfilerne
+#
+# Joergen Friis 10.01.2018
+#
+###############################################################################
+
+baggrund = "Sound/maskinrumStart.ogg"
+assens = "Sound/anloebAssens.ogg"
+hovedmaskine = "Sound/Hovedmaskine.ogg"
+maskintelegraf = "Sound/Maskintelegraf.ogg"
+flute = "Sound/floejteTelegraf.ogg"
+pumpe = "Sound/multiPumpe.ogg"
+dampflute = "Sound/Dampfloejte.ogg"
