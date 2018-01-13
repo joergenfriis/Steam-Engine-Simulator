96037ffe2f47afe67a12e45c4f76b69076b5bd7c
Joergen Friis <joergenfriis@msn.com>
2018-01-13 16:04:11 +0100

    Add files via upload

--- Sound/lyd.py
+++ Sound/lyd.py
@@ -2,14 +2,99 @@
 #
 # modul, der indeholder referencer til alle lydfilerne
 #
-# Joergen Friis 10.01.2018
+# Joergen Friis 13.01.2018
 #
 ###############################################################################
 
 baggrund = "Sound/maskinrumStart.ogg"
 assens = "Sound/anloebAssens.ogg"
 hovedmaskine = "Sound/Hovedmaskine.ogg"
+langsom = "Sound/langsom.ogg"
+halvKraft = "Sound/halvKraft.ogg"
+fuldKraft = "Sound/fuldKraft.ogg"
 maskintelegraf = "Sound/Maskintelegraf.ogg"
 flute = "Sound/floejteTelegraf.ogg"
 pumpe = "Sound/multiPumpe.ogg"
 dampflute = "Sound/Dampfloejte.ogg"
+vand =
+damp = "Sound/dampUd.ogg"
+luft = "Sound/luftUd.ogg"
+vandpjask =
+sikkerhedsventil =
+maskinpiv =
+maskinhaveri =
+
+# kommando #101: "Skipper her - I skal vaere klar til afgang om 5 minutter"
+skipper01 =
+
+# kommando #102: ”200 meter til havnemolen”
+skipper02 =
+
+# kommando #103: ”Skipper her – Så kan I godt bakke af. Tak for sejladsen.”
+skipper03 =
+
+# kommando #104: ” Du kommer for lidt på dækket, og kan åbenbart ikke se forskel på for og agter på et skib.”
+skipper04 =
+
+# kommando #105: ”Bak for helvede”
+skipper05 =
+
+# kommando #106: ”Frem for helvede”
+skipper06 =
+
+# kommando #107: ”Tror du at det er et hvilehjem, det her?”
+skipper07 =
+
+# kommando #108: ”Tag det roligt, vi skal jo ikke smadre det hele”
+skipper08 =
+
+# kommando #109: ”Fuld kraft frem”
+skipper09 =
+
+# kommando #110: ”Halv kraft frem”
+skipper10 =
+
+# kommando #111: ”Ganske langsomt frem”
+skipper11 =
+
+# kommando #112: ”Fuld kraft bak”
+skipper12 =
+
+# kommando #113: ”Halv kraft bak”
+skipper13 =
+
+# kommando #114: ”Ganske langsomt bak”
+skipper14 =
+
+# kommando #115: ”Stop for helvede!”
+skipper15 =
+
+# kommando #116: ”Hvad foregår der? Vi kan ikke leve af at ligge her hele dagen!”
+skipper16 =
+
+# kommando #201: ”Chiefen her – det vil være en god ide at skovle noget kul i ovnen”
+chief01 = "Sound/chief01.ogg"
+
+# kommando #202: ”Chiefen her – du skal åbne Damp Ud hanen for at få luften ud af kedlen. Du skal lukke den igen når du hører at dampen kommer”
+chief02 = "Sound/chief02.ogg"
+
+# kommando #203: ”Chiefen her – Du skal give den noget mere underluft for at få kullene til at give mere varme”
+chief03 = "Sound/chief03.ogg"
+
+# kommando #204: ”Chiefen her – Du skal lukke lågen så den kun er 10 % åben når du ikke fyrer, så brænder kullene bedst.”
+chief04 = "Sound/chief04.ogg"
+
+# kommando #205: ”Chiefen her – du har for meget tryk på kedlen. Luk for primærluft og luk indfyringslågen helt på. Hvis det ikke er nok, så må du lukke lidt damp ud.”
+chief05 = "Sound/chief05.ogg"
+
+# kommando #206: ”Chiefen her – Du skal pumpe noget olie op i tanken, ellers ødelægger du maskinen.”
+chief06 = "Sound/chief06.ogg"
+
+# kommando #207: ”Chiefen her – du skal justere vandstanden i kedlen.”
+chief07 = "Sound/chief07.ogg"
+
+# kommando #208: ”Chiefen her – Du skal lukke helt op for kondensator ind, så du får størst mulig køling af dampen”
+chief08 = "Sound/chief08.ogg"
+
+# kommando #209: "Chiefen her – du skal lukke helt op for kondensator ud, så vandet kan komme tilbage til motoren”.
+chief09 = "Sound/chief09.ogg"
