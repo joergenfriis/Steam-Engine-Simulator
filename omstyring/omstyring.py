dda3d302aa04c420f564ccad345f464d8f1ab402
Joergen Friis <joergenfriis@msn.com>
2018-01-26 16:12:12 +0100

    Add files via upload

--- omstyring/omstyring.py
+++ omstyring/omstyring.py
@@ -10,19 +10,34 @@
 # Byte 1 indeholder et tal mellem 0 og 255, der angiver omstyringshaandtagets stilling.
 # 0 er fuld kraft frem og 255 er fuld kraft bak,
 #
-# Joergen Friis 15.11.2017
+# Joergen Friis 26.01.2018
 #
 #######################################################################################
 
 import smbus
 import time
+import sys
 
 bus = smbus.SMBus(1)
 
 def Read_omstyring():
-    print('Omstyring read')
-    data = bus.read_i2c_block_data(0x29, 0)
-    status = data[0]
-    omstyring = data[1]
+    # Try up to 30 times on a failure
+    Success = False
+    caught_exception = None
+    for _ in range(30):
+        try:
+            data = bus.read_i2c_block_data(0x29, 0)
+            Success = True
+            break
+        except:
+            print("Unexpected error: ",sys.exc_info() [0])
+            time.sleep(1)
+    if not Success:
+        print("Failed after 30 retries")
+    if Success:
+        status = data[0]
+        omstyring = data[1]
+        omstyring = int(omstyring*100/255)
+        print('Omstyring read = ',omstyring)
     return omstyring
     
