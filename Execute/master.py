4e08003eeada1b61ba2fce46685a669433bc75a1
Joergen Friis <joergenfriis@msn.com>
2018-02-05 09:44:38 +0100

    Add files via upload

---
+++ Execute/master.py
@@ -0,0 +1,54 @@
+# master.py
+#
+# Hovedprogram, der vaelger det relevante underprogram paa baggrund
+# afprogramvaelgerens stilling
+#
+# Joergen Friis 02.02.2018
+#
+##############################################################################
+
+import pygame
+import time
+import simulator
+import demo
+import programvalg
+import powernet
+import model
+import servoTryk
+import servoTemp
+import sikkerhedsventil
+import transport
+import skueglasOlie
+import skueglasKedel
+import servo
+
+
+
+while True:
+    print("Starter program nummer ",programvalg.read())
+    while programvalg.read() == 0:
+        print("Lukker alt ned undtagen programvalg")
+        sikkerhedsventil.sikkerhedsventilOff()
+        transport.TransportStop()
+        model.ModelStop()
+        servoTryk.vis(1,0)
+        servoTemp.vis(50)
+        skueglasOlie.set(50)
+        skueglasKedel.set(70)
+        servo.maskintelegraf_FS()
+        if (pygame.mixer.get_init != None):
+            pygame.mixer.quit()
+        powernet.RelayAlloff()
+        
+            
+    while programvalg.read() == 1:
+        print("Starter demoprogram")
+        skueglasOlie.set(50)
+        skueglasKedel.set(50)
+        if programvalg.read() == 1:
+            demo.demo()
+            
+    while programvalg.read() in [2,3,4]:
+        print("Starter simulatorprogram")
+        simulator.start(programvalg.read())
+            
