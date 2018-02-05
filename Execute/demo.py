9ff4b3203b50a9d914e64472671d18ad0ec59723
Joergen Friis <joergenfriis@msn.com>
2018-02-05 09:41:54 +0100

    Add files via upload

--- Execute/demo.py
+++ Execute/demo.py
@@ -3,7 +3,7 @@
 # Modul, der demonstrerer nogle af simulatorens funktioner,
 # uden at modtage brugerinput.
 #
-# Joergen Friis 10.01.2018
+# Joergen Friis 28.01.2018
 #
 #*****************************************************************************
 
@@ -13,6 +13,7 @@ import powernet
 import model
 import IRremote
 import servo
+import programvalg
 
 def demo():
 
@@ -27,49 +28,74 @@ def demo():
     powernet.Relay5on()     # Taend for lydanlaeg
     time.sleep(1)
     powernet.Relay6on()     # Taend for TV skaerm
+    time.sleep(30)
+    IRremote.TVonOff()
     time.sleep(10)
+    IRremote.TVrightArrow()
+    time.sleep(1)
+    IRremote.TVrightArrow()
+    time.sleep(1)
+    IRremote.TVok()
+    time.sleep(1)
+    IRremote.TVok()
+    time.sleep(1)
+    IRremote.TVrightArrow()
+    time.sleep(1)
+    IRremote.TVrightArrow()
+    time.sleep(1)
+    IRremote.TVrightArrow()
+    time.sleep(1)
+    IRremote.TVok()
 
-    IRremote.TVonOff()      # Start filmen
-    time.sleep(5)
-    IRremote.TVrightArrow()
+    if programvalg.read() != 1:
+        return
+
+    powernet.Relay10on()    # Taend forkontrollampe
     time.sleep(1)
-    IRremote.TVrightArrow()
-    time.sleep(1)
-    IRremote.TVok()
-    time.sleep(1)
-    IRremote.TVok()
-    time.sleep(1)
-    IRremote.TVrightArrow()
-    time.sleep(1)
-    IRremote.TVrightArrow()
-    time.sleep(1)
-    IRremote.TVrightArrow()
-    time.sleep(1)
-    IRremote.TVok()
-    
+
 
     pygame.mixer.init()     # Afspil reallyd fra maskinrummet paa Bjoern
     pygame.mixer.music.load("Sound/dampmaskine.mp3")
+    pygame.mixer.music.set_volume(0.8)
     pygame.mixer.music.play()
 
     servo.maskintelegraf_FF()
     model.ModelRun(100,0) # Styr modellen saa den foelger kommandoerne paa reallyden
 
     time.sleep(42)
+
+    if programvalg.read() != 1:
+        return
+    
     time.sleep(170)
+
+    if programvalg.read() != 1:
+        return
 
     servo.maskintelegraf_HF()
     time.sleep(3)
     model.ModelRun(60,0)
     time.sleep(19)
+
+    if programvalg.read() != 1:
+        return
+    
     servo.maskintelegraf_LF()
     time.sleep(3)
     model.ModelRun(30, 0)
     time.sleep(40)
+
+    if programvalg.read() != 1:
+        return
+    
     servo.maskintelegraf_LF()
     time.sleep(3)
     model.ModelRun(20, 0)
     time.sleep(103)
+
+    if programvalg.read() != 1:
+        return
+    
     servo.maskintelegraf_LB()
     time.sleep(3)
     model.ModelRun(30, 100)
@@ -78,6 +104,10 @@ def demo():
     time.sleep(3)
     model.ModelRun(20, 0)
     time.sleep(42)
+
+    if programvalg.read() != 1:
+        return
+    
     servo.maskintelegraf_LB()
     time.sleep(3)
     model.ModelRun(20, 100)
@@ -86,38 +116,74 @@ def demo():
     time.sleep(3)
     model.ModelRun(20, 0)
     time.sleep(35)
+
+    if programvalg.read() != 1:
+        return
+    
     servo.maskintelegraf_LB()
     time.sleep(3)
     model.ModelRun(20, 100)
     time.sleep(24)
+
+    if programvalg.read() != 1:
+        return
+    
     servo.maskintelegraf_LF()
     time.sleep(3)
     model.ModelRun(20, 0)
     time.sleep(109)
+
+    if programvalg.read() != 1:
+        return
+    
     servo.maskintelegraf_LB()
     time.sleep(3)
     model.ModelRun(20, 100)
     time.sleep(25)
+
+    if programvalg.read() != 1:
+        return
+    
     servo.maskintelegraf_LF()
     time.sleep(3)
     model.ModelRun(20, 0)
     time.sleep(28)
+
+    if programvalg.read() != 1:
+        return
+    
     servo.maskintelegraf_HB()
     time.sleep(3)
     model.ModelRun(40, 100)
     time.sleep(24)
+
+    if programvalg.read() != 1:
+        return
+    
     servo.maskintelegraf_LF()
     time.sleep(3)
     model.ModelRun(20, 0)
     time.sleep(67)
+
+    if programvalg.read() != 1:
+        return
+    
     servo.maskintelegraf_LB()
     time.sleep(3)
     model.ModelRun(20, 100)
     time.sleep(14)
     servo.maskintelegraf_LF()
     time.sleep(3)
+
+    if programvalg.read() != 1:
+        return
+    
     model.ModelRun(20, 0)
     time.sleep(225)
+
+    if programvalg.read() != 1:
+        return
+    
     servo.maskintelegraf_FS()
     time.sleep(3)
     model.ModelRun(0, 50)
@@ -127,10 +193,6 @@ def demo():
 
     pygame.mixer.music.stop()
 
-    IRremote.TVonOff()
 
-    powernet.Relay5off()
-    time.sleep(1)
-    powernet.Relay6off()
 
 
